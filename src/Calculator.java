e0410c6bbb1c2e0378898992b6cd1b805ac56579
Marine <marine.lacourie@gmail.com>
2016-03-31 10:03:32 +0800

    Add question to user end of program

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main (String []args) {
-        // show the user the possibilities
         int operationWanted = 0;
         float number1 = 0;
         float number2 = 0;
@@ -19,7 +18,7 @@ public class Calculator {
         Scanner inputOperation = new Scanner(System.in);
         operationWanted = inputOperation.nextInt();
 
-        while (operationWanted != 1 && operationWanted != 2 && operationWanted != 3 && operationWanted != 4){
+        while (operationWanted != 1 && operationWanted != 2 && operationWanted != 3 && operationWanted != 4) {
             System.out.println("Wrong input. Please enter a valid entry (1, 2, 3 or 4):");
             operationWanted = inputOperation.nextInt();
         }
@@ -48,6 +47,68 @@ public class Calculator {
         System.out.println("------------------------------------------------------------------------------");
         System.out.println("The result of your operation is : " + result);
         System.out.println("------------------------------------------------------------------------------");
+
+
+        //Ask operation again
+        System.out.println("Do you want to do another operation? YES/NO");
+        Scanner nextOperation = new Scanner(System.in);
+        String answerNextOperation = nextOperation.nextLine();
+        String answerNextNextOperation = "";
+
+        if (answerNextOperation.equalsIgnoreCase("YES")) {
+            do {
+                System.out.println("------------------------------------------------------------------------------");
+                System.out.println("Hi, please enter the digit corresponding to the operation you want to do:");
+                System.out.println("    1- Add");
+                System.out.println("    2- Substract");
+                System.out.println("    3- Mutiply");
+                System.out.println("    4- Devide");
+                System.out.println("------------------------------------------------------------------------------");
+
+                operationWanted = inputOperation.nextInt();
+
+                while (operationWanted != 1 && operationWanted != 2 && operationWanted != 3 && operationWanted != 4) {
+                    System.out.println("Wrong input. Please enter a valid entry (1, 2, 3 or 4):");
+                    operationWanted = inputOperation.nextInt();
+                }
+
+                System.out.println("Enter the 1st number");
+                number1 = inputNumbers.nextInt();
+
+                System.out.println("Enter the 2nd number");
+                number2 = inputNumbers.nextInt();
+
+                if (operationWanted == 1) {
+                    result = add(number1, number2);
+
+                } else if (operationWanted == 2) {
+                    result = substract(number1, number2);
+
+                } else if (operationWanted == 3) {
+                    result = multiply(number1, number2);
+
+                } else if (operationWanted == 4) {
+                    result = devide(number1, number2);
+                }
+
+                System.out.println("------------------------------------------------------------------------------");
+                System.out.println("The result of your operation is : " + result);
+                System.out.println("------------------------------------------------------------------------------");
+
+                System.out.println("Do you want to do another operation? YES/NO");
+                answerNextNextOperation = nextOperation.nextLine();
+            } while ((answerNextNextOperation.equalsIgnoreCase("YES")));
+            if (answerNextNextOperation.equalsIgnoreCase("NO")) {
+                System.out.println("Thank you for having used this awesome Calculator");
+            } else {
+                System.out.println("Wrong input. Please run the program again.");
+            }
+
+        } else if (answerNextOperation.equalsIgnoreCase("NO") ||answerNextNextOperation.equalsIgnoreCase("NO")) {
+            System.out.println("Thank you for having used this awesome Calculator");
+        } else {
+            System.out.println("Wrong input. Please run the program again.");
+        }
     }
 
     public static float add(float a, float b){
